14a69e270d5c16a54048434be0b7e7a594578123
David Albert <davidbalbert@gmail.com>
2025-06-20 13:15:46 -0700

    working? cross glibc.mk

--- mk/glibc.mk
+++ mk/glibc.mk
@@ -1,27 +1,33 @@
-bootstrap-glibc: $(BOOTSTRAP_OUT_DIR)/.glibc.installed
-glibc: $(TARGET_OUT_DIR)/.glibc.installed
+bootstrap-glibc: $(BOOTSTRAP_BUILD_DIR)/.glibc.installed
+glibc: $(TARGET_BUILD_DIR)/.glibc.installed
 
+%/.glibc.installed: CFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$*=.
+%/.glibc.installed: CXXFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$*=.
+%/.glibc.installed: SOURCE_DATE_EPOCH = $(shell cat $*/glibc/src/.timestamp 2>/dev/null || echo 1)
+
+$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: HOST_TRIPLE := $(BUILD_TRIPLE)
+$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: TARGET_TRIPLE := $(BUILD_TRIPLE)
+# there's no bootstrap sysroot
+$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: SYSROOT := $(BUILD_SYSROOT)
 $(BOOTSTRAP_BUILD_DIR)/.glibc.installed: PATH := $(BOOTSTRAP_PREFIX)/bin:$(ORIG_PATH)
-$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: CFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$(BOOTSTRAP_BUILD_DIR)=.
-$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: CXXFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$(BOOTSTRAP_BUILD_DIR)=.
-$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: SOURCE_DATE_EPOCH := $(shell cat $(BOOTSTRAP_BUILD_DIR)/glibc/src/.timestamp 2>/dev/null || echo 1)
-$(BOOTSTRAP_BUILD_DIR)/.glibc.installed: SYSROOT := $(BOOTSTRAP_SYSROOT)
 
-$(CROSS_BUILD_DIR)/.glibc.installed: PATH := $(CROSS_PREFIX)/bin:$(ORIG_PATH)
-$(CROSS_BUILD_DIR)/.glibc.installed: CFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$(CROSS_BUILD_DIR)=.
-$(CROSS_BUILD_DIR)/.glibc.installed: CXXFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$(CROSS_BUILD_DIR)=.
-$(CROSS_BUILD_DIR)/.glibc.installed: SOURCE_DATE_EPOCH := $(shell cat $(CROSS_BUILD_DIR)/glibc/src/.timestamp 2>/dev/null || echo 1)
+$(BUILD_BUILD_DIR)/.glibc.installed: HOST_TRIPLE := $(BUILD_TRIPLE)
+$(BUILD_BUILD_DIR)/.glibc.installed: TARGET_TRIPLE := $(BUILD_TRIPLE)
+$(BUILD_BUILD_DIR)/.glibc.installed: SYSROOT := $(BUILD_SYSROOT)
+$(BUILD_BUILD_DIR)/.glibc.installed: PATH := $(BUILD_PREFIX)/bin:$(ORIG_PATH)
+
+$(CROSS_BUILD_DIR)/.glibc.installed: HOST_TRIPLE := $(BUILD_TRIPLE)
+$(CROSS_BUILD_DIR)/.glibc.installed: TARGET_TRIPLE := $(HOST_TRIPLE)
 $(CROSS_BUILD_DIR)/.glibc.installed: SYSROOT := $(CROSS_SYSROOT)
+$(CROSS_BUILD_DIR)/.glibc.installed: PATH := $(CROSS_PREFIX)/bin:$(ORIG_PATH)
 
-$(TARGET_BUILD_DIR)/.glibc.installed: PATH := $(TARGET_PREFIX)/bin:$(ORIG_PATH)
-$(TARGET_BUILD_DIR)/.glibc.installed: CFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$(TARGET_BUILD_DIR)=.
-$(TARGET_BUILD_DIR)/.glibc.installed: CXXFLAGS := -O2 -g -ffile-prefix-map=$(SRC_DIR)=. -ffile-prefix-map=$(TARGET_BUILD_DIR)=.
-$(TARGET_BUILD_DIR)/.glibc.installed: SOURCE_DATE_EPOCH := $(shell cat $(TARGET_BUILD_DIR)/glibc/src/.timestamp 2>/dev/null || echo 1)
 $(TARGET_BUILD_DIR)/.glibc.installed: SYSROOT := $(TARGET_SYSROOT)
+$(TARGET_BUILD_DIR)/.glibc.installed: PATH := $(CROSS_PREFIX)/bin:$(ORIG_PATH)
 
-$(BOOTSTRAP_BUILD_DIR)/.glibc.configured: $(BOOTSTRAP_OUT_DIR)/.gcc.installed $(BOOTSTRAP_OUT_DIR)/.linux-headers.installed
-$(CROSS_BUILD_DIR)/.glibc.configured: $(CROSS_OUT_DIR)/.gcc.installed $(BOOTSTRAP_OUT_DIR)/.linux-headers.installed
-$(TARGET_BUILD_DIR)/.glibc.configured: $(TARGET_OUT_DIR)/.gcc.installed $(BOOTSTRAP_OUT_DIR)/.linux-headers.installed
+$(BOOTSTRAP_BUILD_DIR)/.glibc.configured: $(BOOTSTRAP_BUILD_DIR)/.gcc.installed $(BOOTSTRAP_BUILD_DIR)/.linux-headers.installed
+$(BUILD_BUILD_DIR)/.glibc.configured: $(BUILD_BUILD_DIR)/.gcc.installed $(BOOTSTRAP_BUILD_DIR)/.linux-headers.installed
+$(CROSS_BUILD_DIR)/.glibc.configured: $(CROSS_BUILD_DIR)/.gcc.installed $(BOOTSTRAP_BUILD_DIR)/.linux-headers.installed
+$(TARGET_BUILD_DIR)/.glibc.configured: $(TARGET_BUILD_DIR)/.gcc.installed $(BOOTSTRAP_BUILD_DIR)/.linux-headers.installed
 
 GLIBC_CONFIG = \
 	--prefix=/usr \
@@ -29,6 +35,8 @@ GLIBC_CONFIG = \
 	--enable-kernel=$(GLIBC_KERNEL_VERSION) \
 	--with-headers=$(SYSROOT)/usr/include \
 	libc_cv_slibdir=/usr/lib
+
+.PRECIOUS: %/.glibc.configured %/.glibc.compiled
 
 %/.glibc.configured: $(SRC_DIR)/glibc-$(GLIBC_VERSION)
 	mkdir -p $*/glibc/build $(SYSROOT)
