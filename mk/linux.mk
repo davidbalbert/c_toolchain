d41e8721164c76cdf1222db945bb840346aca408
David Albert <davidbalbert@gmail.com>
2025-06-21 12:34:58 -0700

    Fix linux headers

--- mk/linux.mk
+++ mk/linux.mk
@@ -1,15 +1,11 @@
-%/.linux-headers.installed: PATH := $(ORIG_PATH)
+build/%/.linux-headers.installed: PATH := $(ORIG_PATH)
 
-$(BOOTSTRAP_BUILD_DIR)/.linux-headers.installed: BUILD_DIR := $(BOOTSTRAP_BUILD_DIR)
-$(CROSS_BUILD_DIR)/.linux-headers.installed: BUILD_DIR := $(CROSS_BUILD_DIR)
-$(TARGET_BUILD_DIR)/.linux-headers.installed: BUILD_DIR := $(TARGET_BUILD_DIR)
+build/%/.linux-headers.installed: BUILD_DIR = $(abspath build/$*)
+build/%/.linux-headers.installed: TARGET_ARCH = $(word 1,$(subst -, ,$(TARGET_TRIPLE)))
+build/%/.linux-headers.installed: KERNEL_ARCH = $(if $(filter x86_64,$(TARGET_ARCH)),x86_64,$(if $(filter aarch64,$(TARGET_ARCH)),arm64,$(error Unsupported architecture: $(TARGET_ARCH))))
+build/%/.linux-headers.installed: SYSROOT = $(OUT_DIR)/$*/sysroot
 
-%/.linux-headers.installed: TARGET_ARCH = $(word 1,$(subst -, ,$(TARGET_TRIPLE)))
-%/.linux-headers.installed: KERNEL_ARCH = $(if $(filter x86_64,$(TARGET_ARCH)),x86_64,$(if $(filter aarch64,$(TARGET_ARCH)),arm64,$(error Unsupported architecture: $(TARGET_ARCH))))
-%/.linux-headers.installed: SYSROOT = $(patsubst $(BUILD_DIR)/%,$(OUT_DIR)/%/sysroot,$(BUILD_DIR))
-$(BOOTSTRAP_BUILD_DIR)/.linux-headers.installed: SYSROOT := $(BUILD_SYSROOT)
-
-%/.linux-headers.installed: $(SRC_DIR)/linux-$(LINUX_VERSION)
+build/%/.linux-headers.installed: $(SRC_DIR)/linux-$(LINUX_VERSION)
 	$(eval TMPDIR := $(shell mktemp -d))
 
 	mkdir -p $(BUILD_DIR)/linux-headers/build $(SYSROOT)
